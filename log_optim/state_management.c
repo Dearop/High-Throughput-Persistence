d2ed3fba32cbd2c0e4095b78d8422f6996bfe69f
Dearop <rocamar564p@gmail.com>
2025-05-23 16:19:36 +0200

    Add recovery timing

--- log_optim/state_management.c
+++ log_optim/state_management.c
@@ -12,7 +12,7 @@
 #define BATCH_SIZE        (1 << 16)  // 2^16 transactions per batch
 #define INITIAL_BALANCE   1000000UL
 #define MAX_ACCOUNTS      500000000UL
-#define TOTAL_BATCHES     125000         // Process 50 batches total
+#define TOTAL_BATCHES     5000         // Process 50 batches total
 #define MAX_LOG_BATCHES   100          // Snapshot every 5 batches
 
 // Filenames
