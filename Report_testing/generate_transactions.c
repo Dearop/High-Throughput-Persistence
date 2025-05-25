1bc893d10a6d22a443cab52f39fca71a9426da82
Dearop <rocamar564p@gmail.com>
2025-05-25 15:29:55 +0200

    Report testing

--- Report_testing/generate_transactions.c
+++ Report_testing/generate_transactions.c
@@ -6,7 +6,7 @@
 
 // --- Definitions ---
 #define BATCH_SIZE          (1ULL << 16)
-#define NUMBER_OF_BATCHES   5000UL
+#define NUMBER_OF_BATCHES   5000
 #define TOTAL_TRANSACTIONS  (BATCH_SIZE * NUMBER_OF_BATCHES)
 #define SMALL_ACCOUNT_COUNT  5000000UL       // Target number of accounts
 
